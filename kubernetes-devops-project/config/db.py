d1c8b844102463ce093455f4caa4f51826a9728d
RAPHTER1 <raphael.faryolax@gmail.com>
2025-02-20 17:31:17 +0000

    Commented ressources requests - readme.md

--- kubernetes-devops-project/config/db.py
+++ kubernetes-devops-project/config/db.py
@@ -1,7 +1,14 @@
 from sqlalchemy import create_engine, MetaData
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://admin:password@db:5432/storedb")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+DATABASE_HOST = os.getenv("DATABASE_HOST")
+DATABASE_PORT = os.getenv("DATABASE_PORT")
+
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{POSTGRES_DB}"
+
 engine = create_engine(DATABASE_URL)
 
 meta = MetaData()
